429d219bd19a04546606dda096224000f261929a
Azrael Reyes <114135920+Aziwan@users.noreply.github.com>
2025-03-31 23:29:11 +0800

    Delete public directory

--- public/pages/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start();
-session_unset();  // Unset all session variables
-session_destroy(); // Destroy the session
-header('Location: /?page=welcome'); // Redirect to the login page (or your desired page)
-exit();
-?>
